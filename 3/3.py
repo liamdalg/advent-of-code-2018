59e063a19d95cdaffac5fe58135af40ef2f80c4b
liamdalg <liamdalg99@gmail.com>
2018-12-09 01:11:48 +0000

    day 3 but it sucks

---
+++ 3/3.py
@@ -0,0 +1,17 @@
+import sys
+# strip down into x y w h 
+stripped = [ x[x.index('@') + 1:].replace(':', '').replace(',', ' ').replace('x', ' ').strip().split(' ') for x in sys.stdin.readlines() ]
+# now parse into (x,y) and (x', y') (wtf)
+claims = [ ((int(x[0]), int(x[0]) + int(x[2])), ((int(x[1]), int(x[1]) + int(x[3])))) for x in stripped ]
+
+claimed = {}
+for c in claims:
+    for x in range(c[0][0], c[0][1]):
+        for y in range(c[1][0], c[1][1]):
+            if (x, y) in claimed:
+                claimed[(x, y)] = 1
+            else: claimed[(x, y)] = 0
+
+print(sum(claimed.values()))
+
+#part two
