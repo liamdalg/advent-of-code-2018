77ec81881aa135cdbee0dd7a0dbb8ba96a5366ac
liamdalg <liamdalg99@gmail.com>
2018-12-13 15:32:54 +0000

    task 2 ezpz

--- 6/6.py
+++ 6/6.py
@@ -1,7 +1,7 @@
 import sys
 from string import ascii_letters
 
-# part one
+# part one AND part two
 coords = [ (int(x.split(',')[0]), int(x.split(',')[1])) for x in sys.stdin.readlines() ]
 
 def dist(a: (int, int), b: (int, int)) -> int:
@@ -20,19 +20,21 @@ def isFinite(x: (int, int), ys: [(int, int)]) -> bool:
     up    = [ x[1] > y[1] and dist_y(y, x) >= dist_x(y, x) for y in ys if x != y ]
     return any(left) and any(right) and any(down) and any(up)
 
-min_x, min_y = min(coords)
 max_x, max_y = max(coords, key=lambda x: x[0])[0], max(coords, key=lambda x: x[1])[1]
 
-width = max_x - min_x + 1
-height = max_y - min_y + 1
+width = max_x
+height = max_y
 
 grid = [ '' ] * height
 areas = { c: 0 for c in coords}
 chars = { coords[i]: ascii_letters[i] for i in range(len(coords)) }
 
+safe = 0
 for i in range(height):
     for j in range(width):
-        distances = { x: dist((j + min_x, i + min_y ), x) for x in coords }
+        distances = { x: dist((j, i), x) for x in coords }
+        total_dist = sum(distances.values())
+        safe += 1 if total_dist < 10000 else 0
         closest = min(distances, key=distances.get)
         closest_dist = distances[closest]
         # if owned by one
@@ -44,6 +46,7 @@ for i in range(height):
 finites = { k: v for (k, v) in areas.items() if isFinite(k, coords) }
 max_area = max(finites, key=finites.get)
 print('{}: {}'.format(max_area, areas[max_area]))
+print('Safe size: {}'.format(safe))
 
 with open('out.txt', 'w+') as f:
     for l in grid:
