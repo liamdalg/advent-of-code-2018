c974fbe57ed8336e0a335f039de0dc5ba8c96f15
Liam <liamdalg99@gmail.com>
2018-12-13 13:32:52 +0000

    clean

--- 6/6.py
+++ 6/6.py
@@ -14,10 +14,10 @@ def dist_y(a: (int, int), b: (int, int)) -> int:
     return abs(b[1] - a[1])
 
 def isFinite(x: (int, int), ys: [(int, int)]) -> bool:
-    left  = [ x[0] > y[0] and abs(y[1] - x[1]) <= abs(y[0] - x[0]) for y in ys if x != y ]
-    right = [ x[0] < y[0] and abs(y[1] - x[1]) <= abs(y[0] - x[0]) for y in ys if x != y ]
-    down  = [ x[1] < y[1] and abs(y[1] - x[1]) >= abs(y[0] - x[0]) for y in ys if x != y ]
-    up    = [ x[1] > y[1] and abs(y[1] - x[1]) >= abs(y[0] - x[0]) for y in ys if x != y ]
+    left  = [ x[0] > y[0] and dist_y(y, x) <= dist_x(y, x) for y in ys if x != y ]
+    right = [ x[0] < y[0] and dist_y(y, x) <= dist_x(y, x) for y in ys if x != y ]
+    down  = [ x[1] < y[1] and dist_y(y, x) >= dist_x(y, x) for y in ys if x != y ]
+    up    = [ x[1] > y[1] and dist_y(y, x) >= dist_x(y, x) for y in ys if x != y ]
     return any(left) and any(right) and any(down) and any(up)
 
 min_x, min_y = min(coords)
@@ -26,18 +26,16 @@ max_x, max_y = max(coords, key=lambda x: x[0])[0], max(coords, key=lambda x: x[1
 width = max_x - min_x + 1
 height = max_y - min_y + 1
 
-print(width, height)
-
 grid = [ '' ] * height
 areas = { c: 0 for c in coords}
 chars = { coords[i]: ascii_letters[i] for i in range(len(coords)) }
-print(chars)
 
 for i in range(height):
     for j in range(width):
         distances = { x: dist((j + min_x, i + min_y ), x) for x in coords }
         closest = min(distances, key=distances.get)
         closest_dist = distances[closest]
+        # if owned by one
         if sum(v == closest_dist for v in distances.values()) == 1:
             grid[i] += chars[closest]
             areas[closest] += 1
