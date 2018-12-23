2d9bc32f3722e46ca10c735b74deecd1b44f0a5d
liamdalg <liamdalg99@gmail.com>
2018-12-23 16:03:00 +0000

    wtf

--- 12/12.py
+++ 12/12.py
@@ -5,22 +5,34 @@ def parse(x: [str]) -> (str, [str]):
     will_live = [ y[:5] for y in x if '=> #' in y ]
     return (init, will_live)
 
-state, will_live = parse(sys.stdin.readlines())
-# I'm lazy so let's just pad for now
-state = state.ljust(200, '.').rjust(300, '.')
+def calc_score(state: str, offset: int) -> int:
+    score = 0
+    for i, v in enumerate(state):
+        if v == '#': 
+            score += (i - offset)
+    return score
 
-for i in range(20):
+state, will_live = parse(sys.stdin.readlines())
+state = '.....' + state + '....'
+
+prev_score = None
+cur_score = None
+offset = 5
+for i in range(150):
     new_state = ''
     for x in range(len(state)):
         if state[x-2:x+3] in will_live:
             new_state += '#'
         else: new_state += '.'
+    if new_state[:3] == '..#':
+        offset += 2
+        new_state = '..' + new_state
+    if new_state[-3:] == '#..':
+        new_state = new_state + '..'
     state = new_state
-
-score = 0
-for i, v in enumerate(state):
-    if v == '#': 
-        score += (i - 100)
+    prev_score = cur_score
+    cur_score = calc_score(state, offset)
+    if prev_score and cur_score:
+        print('Generation #{}: Score {}, Diff {}'.format(i + 1, cur_score, cur_score - prev_score))
 
 print(state)
-print(score)
